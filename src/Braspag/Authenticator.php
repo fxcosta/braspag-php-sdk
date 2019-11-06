ec9bbe5ddbcd009343df829310a96efaee069e31
Joan Marcos <joan.santos@soterotech.com.br>
2019-11-06 18:57:07 -0300

    Correcao metodo json.

--- src/Braspag/Authenticator.php
+++ src/Braspag/Authenticator.php
@@ -161,7 +161,7 @@ class Authenticator
         switch ($statusCode) {
             case 200:
             case 201:
-                $unserialized = $this->json_decode($responseBody);
+                $unserialized = json_decode($responseBody);
                 break;
             case 400:
                 $exception = null;
