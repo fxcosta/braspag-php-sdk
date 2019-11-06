c6f3e8e64b493842774b6d2ecd541940d4405779
Joan Marcos <joan.santos@soterotech.com.br>
2019-11-06 10:48:52 -0300

    Correcao de retorno de mensagem de erro.

--- src/Braspag/API/Request/AbstractRequest.php
+++ src/Braspag/API/Request/AbstractRequest.php
@@ -148,8 +148,8 @@ abstract class AbstractRequest
                 $exception = null;
                 $response = json_decode($responseBody);
 
-                foreach ($response as $error) {
-                    $braspagError = new BraspagError($error->Message, $error->Code);
+                foreach ($response->Errors as $error) {
+                    $braspagError = new BraspagError($error->Message, $statusCode);
                     $exception = new BraspagRequestException('Request Error', $statusCode, $exception);
                     $exception->setBraspagError($braspagError);
                 }
